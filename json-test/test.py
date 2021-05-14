a021d4aabf98b2a6c18704a6c02ba38f6197b685
stuayu <60843145+stuayu@users.noreply.github.com>
2021-05-14 14:36:34 +0900

    update test.py

--- json-test/test.py
+++ json-test/test.py
@@ -1,8 +1,17 @@
 import json
 import requests
 
-url = "http://cal.syoboi.jp/json.php"
+def search_title(year):
+    for i in range(1, 8000):
+        try:
+            if (json_data['Titles'][str(i)]['FirstYear'] == str(year)):
+                print(json_data['Titles'][str(i)]['Title'])
+        except KeyError:
+            pass
+
+url = "https://cal.syoboi.jp/json.php?Req=TitleLarge"
 session = requests.Session()
 data = session.get(url)
 json_data = json.loads(data.text)
-print(json_data['Titles']['1'])
+
+search_title(2021)
